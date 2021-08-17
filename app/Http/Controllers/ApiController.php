8d8a37543865519f5ecf1c587f755dc667d85cbb
Yaroslav <yarglivenko@gmail.com>
2021-08-17 10:26:09 +0300

    Init

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Class ApiController
+ *
+ * @package App\Http\Controllers
+ */
+class ApiController extends Controller
+{
+    /**
+     * @param  mixed  $data
+     *
+     * @return JsonResponse
+     */
+    public function respondSuccess($data = []): JsonResponse
+    {
+        return response()->json($data, JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * @param  mixed  $data
+     *
+     * @return JsonResponse
+     */
+    public function respondCreated($data = []): JsonResponse
+    {
+        return response()->json($data, JsonResponse::HTTP_CREATED);
+    }
+
+    /**
+     * @param  mixed  $data
+     *
+     * @return JsonResponse
+     */
+    public function respondAccepted($data = []): JsonResponse
+    {
+        return response()->json($data, JsonResponse::HTTP_ACCEPTED);
+    }
+
+    /**
+     * @param mixed $data
+     *
+     * @return JsonResponse
+     */
+    public function respondUnprocessableEntity($data=null): JsonResponse
+    {
+        return response()->json($data, JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
